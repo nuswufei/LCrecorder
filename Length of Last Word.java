72b83713cf47921ecbeb09f67ffd1abfd6823a81
nuswufei <nuswufei@gmail.com>
2015-01-09 12:41:03 -0800

    Create Length of Last Word.java

---
+++ Length of Last Word.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        s = s.trim();
+        for(int i =  s.length() - 1; i>=0; --i) {
+            if(s.charAt(i) == ' ') break;
+            ++length;
+        }
+        return length;
+    }
+}
