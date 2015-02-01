ea2f1440ab0f9f1f39b924dc564657a0e3a90130
nuswufei <nuswufei@gmail.com>
2015-01-31 17:18:56 -0800

    Update Search in Rotated Sorted Array II.java

--- Search in Rotated Sorted Array II.java
+++ Search in Rotated Sorted Array II.java
@@ -1,5 +1,46 @@
 public class Solution {
     public boolean search(int[] A, int target) {
+        int minIndex = 0;
+        int left = 0;
+        int right = A.length - 1;
+        while(left <= right) {
+            if(A[left] < A[right]) {
+                minIndex = left;
+                break;
+            }
+            int mid = (left + right) >> 1;
+            if(mid > 0 && A[mid - 1] > A[mid]) {
+                minIndex = mid;
+                break;
+            }
+            if(left > 0 && A[left - 1] > A[left]) {
+                minIndex = left;
+                break;
+            }
+            else if(A[mid] < A[left]) {
+                right = mid - 1;
+            }
+            else if(A[mid] == A[left]) ++left;
+            
+            else {
+                left = mid + 1;
+            }
+        }
+        left = 0;
+        right = A.length - 1;
+        while(left <= right) {
+            int mid = (left + right) >> 1;
+            int midInA = (mid + minIndex) % A.length;
+            if(A[midInA] == target) return true;
+            if(A[midInA] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return false;
+    }
+}
+
+public class Solution {
+    public boolean search(int[] A, int target) {
         return sr(A, target, 0, A.length - 1);
     }
     public boolean sr(int[] A, int target, int left, int right) {
